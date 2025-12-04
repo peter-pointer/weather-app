0999778b35bd73b7e5792b82f8ef2d35a3e0048a
Pedro Florit Docavo <florit-docavo@erc-system.com>
2025-12-04 14:32:10 +0100

    pruebas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Como estais chavales????</h1>
+      <h1>Vite + React + Como estais chavalotes!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
